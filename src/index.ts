78eb9e9becf17b6678e557f0207ec706ae042c67
Jonathan Miles <jonathandanielmiles@gmail.com>
2026-04-16 15:31:33 +0700

    Fix package name

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export * from './modules/permissions/components/PermissionsCarousel';
+export * from './modules/permissions/components/PermissionsPrompt';
 export * from './modules/permissions/components/PermissionsProvider';
-export * from './modules/permissions/components/PermissionsWarning';
 export * from './modules/permissions/hooks/usePermissionRequest';
+export * from './modules/permissions/hooks/usePermissionsContext';
 export * from './modules/permissions/types';
