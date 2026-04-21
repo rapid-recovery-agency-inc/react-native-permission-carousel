fb7713a135fb1737fe0d35fec40730daf9fbc596
Jonathan Miles <jonathandanielmiles@gmail.com>
2026-04-21 07:41:40 +0700

    Tidy

--- src/shared/i18n/index.ts
+++ src/shared/i18n/index.ts
@@ -6,7 +6,7 @@ import { messages } from './locales/en';
 export const PERMISSIONS_I18N_NAMESPACE = 'permissions';
 
 export function addTranslationsTo(i18nInstance: i18n): void {
-  i18next.addResourceBundle('en', PERMISSIONS_I18N_NAMESPACE, messages.permissions, true, true);
+  i18nInstance.addResourceBundle('en', PERMISSIONS_I18N_NAMESPACE, messages.permissions, true, true);
 }
 
 export const t = i18next.t.bind(i18next);
